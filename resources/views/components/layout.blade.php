b33c8692ff891208880c40240832ba949a58907f
benito <benito11kpo@gmail.com>
2026-04-11 22:47:35 -0300

    añadi logo y cambie lugar carpeta imagenes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
 <body class="bg-light d-flex flex-column min-vh-100">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/">RETRO STORE</a>
+            <a class="navbar-brand" href="/"> <img src="{{ asset('img/logo.png')}}" alt="Logo"> </a>
 
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto">
