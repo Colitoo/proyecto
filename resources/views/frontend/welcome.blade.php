ae2e2947bbd5afec23d266d66fc138f7e73f0f0a
benito <benito11kpo@gmail.com>
2026-04-14 14:09:00 -0300

    segunda version del inicio

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -5,30 +5,72 @@
     </div>
 
 
-    <section class="container mt-4">
+    <section class="prod-mas-vendidos container mt-4">
         <h2>Productos más vendidos</h2>
-        <div id="carouselExample" class="carousel slide">
+
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{asset('img/3DS.jpg')}}" class="d-block w-100" alt="Nintendo 3DS">
+                    <img src="{{asset('img/3DS.jpg')}}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{asset('img/play1.jpg')}}" class="d-block w-100" alt="PlayStation 1">
+                    <img src="{{asset('img/play1.jpg')}}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{asset('img/play2.jpg')}}" class="d-block w-100" alt="PlayStation 2">
+                    <img src="{{asset('img/play2.jpg')}}" class="d-block w-100" alt="...">
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
         <a href="/productos" class="btn btn-primary">Ver más</a>
+    </section>
+
+    <section class="comentarios-container container mt-4">
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{asset('img/mandops2.jpg')}}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 class="text-dark">Juan Perez</h5>
+                        <p class="text-dark">Excelente producto, muy satisfecho con la compra. Los tiempos de entrega fueron rápidos, recomendado.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{asset('img/wii.jpg')}}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 class="text-dark">Maria Garcia</h5>
+                        <p class="text-dark">Producto de alta calidad, es la segunda vez que compro en el sitio y no tuve inconvenientes.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{asset('img/play1.jpg')}}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 class="text-dark">Carlos Rodriguez</h5>
+                        <p class="text-dark">Excelente servicio al cliente y producto de calidad, volveria a comprar sin dudas</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </section>
 
 </x-layout>
